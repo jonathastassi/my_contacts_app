764f05f394d53eca462411b800868d1679034032
Jonathas Silva <jonathas.silva@dextra-sw.com>
2021-01-27 08:56:26 -0300

    tela inicial

--- lib/app/modules/home/pages/initial/initial_page.dart
+++ lib/app/modules/home/pages/initial/initial_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:my_contacts_app/app/shared/layout/drawer_menu.dart';
+import 'package:my_contacts_app/app/shared/components/card_component.dart';
+import 'package:my_contacts_app/app/shared/layout/bottom_menu.dart';
 import 'initial_controller.dart';
 
 class InitialPage extends StatefulWidget {
@@ -20,9 +21,14 @@ class _InitialPageState extends ModularState<InitialPage, InitialController> {
         title: Text("Início"),
         centerTitle: true,
       ),
-      drawer: DrawerMenu(),
+      bottomNavigationBar: BottomMenu(),
       body: Column(
-        children: <Widget>[],
+        children: <Widget>[
+          CardComponent(
+              child: Text(
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+              title: "Bem-vindo"),
+        ],
       ),
     );
   }
