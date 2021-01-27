5b00c5f134ecdf0660d5eee2b0e6dee34b4e2dcd
Jonathas Silva <jonathas.silva@dextra-sw.com>
2021-01-27 08:56:51 -0300

    BottomAppBar menu

---
+++ lib/app/shared/layout/bottom_menu.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+class BottomMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      shape: CircularNotchedRectangle(),
+      child: Container(
+        height: 50,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: InkWell(
+                onTap: () {
+                  Modular.to.pushReplacementNamed("/home/initial");
+                },
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(Icons.home),
+                    Text("Início"),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 90,
+              height: 50,
+            ),
+            Expanded(
+              child: InkWell(
+                onTap: () {
+                  Modular.to.pushReplacementNamed("/contacts/list");
+                },
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(Icons.person_sharp),
+                    Text("Contatos"),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
